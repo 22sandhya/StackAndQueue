001bfdae971b4fcae82a362508a27527b00b47d5
Admin <sandhyabagale10@gmail.com>
2021-10-06 15:57:54 +0530

    Added:welcome message

---
+++ main/java/com/bidgelabs/DataStracture/StackAndQue.java
@@ -0,0 +1,10 @@
+package com.bidgelabs.DataStracture;
+
+
+public class StackAndQue 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to Stack and queue programs" );
+    }
+}
